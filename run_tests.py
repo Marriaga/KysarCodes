56d40dad90544eedf21afa51554bcf31599ae2ed
Miguel Arriaga <mta2122@columbia.edu>
2018-01-16 19:01:48 -0500

    added function to remove .pyc files from test folder after running tests

--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,14 @@
 import unittest
+import sys
+import os
+
 
 if __name__ == '__main__':
-
     test_suite = unittest.defaultTestLoader.discover('.')
     unittest.TextTestRunner(verbosity=2).run(test_suite)
+    
+    #Delete the .pyc files in test directory
+    dir_name = "test"
+    for fname in os.listdir(dir_name):
+        if fname.endswith(".pyc"):
+            os.remove(os.path.join(dir_name, fname))
