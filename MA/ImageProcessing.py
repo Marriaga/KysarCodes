c49b2aa1fadd9aecb70b836e15f8a01a2bfc5861
Miguel Arriaga <mta2122@columbia.edu>
2018-10-04 18:21:10 -0400

    PIL Draw Import

--- MA/ImageProcessing.py
+++ MA/ImageProcessing.py
@@ -3,7 +3,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 from builtins import range
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 import scipy.optimize as spoptimize
 from scipy.signal import convolve2d
